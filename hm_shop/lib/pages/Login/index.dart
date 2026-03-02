168193a4876108312f13800a3f2f0dff9b28a68a
womendeshijiechongmanzhe <2482207668@qq.com>
2026-03-02 21:59:28 +0800

    index update

--- hm_shop/lib/pages/Login/index.dart
+++ hm_shop/lib/pages/Login/index.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("主页"), centerTitle: true),
-      body: Center(child: Text("主页")),
+      appBar: AppBar(title: Text("登录"), centerTitle: true),
+      body: Center(child: Text("登录")),
     );
   }
 }
--- hm_shop/lib/pages/Main/index.dart
+++ hm_shop/lib/pages/Main/index.dart
@@ -1,18 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/My/index.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Map<String, String>> _tabList = [
+    {
+      "icon": "lib/assets/home.png",
+      "activeIcon": "lib/assets/home-act.png",
+      "text": "首页",
+    },
+    {
+      "icon": "lib/assets/type.png",
+      "activeIcon": "lib/assets/type-act.png",
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/cart.png",
+      "activeIcon": "lib/assets/cart-active.png",
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/my.png",
+      "activeIcon": "lib/assets/my-act.png",
+      "text": "我的",
+    },
+  ];
+
+  List<Widget> _getpageList() {
+    return [HomeView(), CategoryView(), CartView(), MyView()];
+  }
+
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabList[index]["icon"]!, width: 30, height: 30),
+        activeIcon: Image.asset(
+          _tabList[index]["activeIcon"]!,
+          width: 30,
+          height: 30,
+        ),
+        label: _tabList[index]["text"],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("登录"), centerTitle: true),
-      body: Center(child: Text("登录页面")),
+      body: SafeArea(
+        child: IndexedStack(index: _currentIndex, children: _getpageList()),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: _getTabBarWidget(),
+      ),
     );
   }
 }
