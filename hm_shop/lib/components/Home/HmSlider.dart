37cea0c797362c9a8eae3777ff7bcdd43a7127db
womendeshijiechongmanzhe <2482207668@qq.com>
2026-03-03 19:00:51 +0800

    change

--- hm_shop/lib/components/Home/HmSlider.dart
+++ hm_shop/lib/components/Home/HmSlider.dart
@@ -13,11 +13,16 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  int _currentIndex = 0;
+  final CarouselSliderController _carouselSliderController =
+      CarouselSliderController();
+
   Widget _getSliderItem() {
     //获取媒体宽度
     final screenWidth = MediaQuery.of(context).size.width;
 
     return CarouselSlider(
+      carouselController: _carouselSliderController,
       items: List.generate(widget.bannerList.length, (index) {
         return Image.network(
           widget.bannerList[index].imageUrl ?? "",
@@ -29,19 +34,75 @@ class _HmSliderState extends State<HmSlider> {
         viewportFraction: 1.0,
         autoPlay: true,
         autoPlayInterval: Duration(seconds: 3),
+        onPageChanged: (index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 40,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            "搜索....",
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      right: 0,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                _carouselSliderController.animateToPage(index);
+                setState(() {});
+              },
+              child: Container(
+                height: 6,
+                width: _currentIndex == index ? 40 : 10,
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                  color: _currentIndex == index
+                      ? Colors.white
+                      : const Color.fromRGBO(0, 0, 0, 0.3),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_getSliderItem()]);
-
-    // Container(
-    //   color: Colors.deepOrange,
-    //   height: 300,
-    //   alignment: Alignment.center,
-    //   child: Text("轮播图", style: TextStyle(fontSize: 20, color: Colors.yellow)),
-    // );
+    return Stack(children: [_getSliderItem(), _getSearch(), _getDots()]);
   }
 }
